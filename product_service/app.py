1fbc8816317a38aafa63374143f61f475e356979
Xuân Tú <buixuantu01@gmail.com>
2025-01-09 09:50:46 +0700

    fix app product_service

--- product_service/app.py
+++ product_service/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 
+
 # Khởi tạo ứng dụng Flask
 app = Flask(__name__)
 
@@ -9,10 +10,12 @@ products = [
     {"id": 102, "name": "Phone"}
 ]
 
+
 # API: Lấy danh sách sản phẩm
 @app.route('/products', methods=['GET'])
 def get_products():
     return jsonify(products)
+
 
 # API: Thêm sản phẩm mới
 @app.route('/products', methods=['POST'])
@@ -21,7 +24,10 @@ def add_product():
     if any(product['id'] == new_product['id'] for product in products):
         return jsonify({"message": "Product ID already exists!"}), 400
     products.append(new_product)
-    return jsonify({"message": "Product added successfully!", "product": new_product}), 201
+    return jsonify(
+        {"message": "Product added successfully!", "product": new_product}
+    ), 201
+
 
 # Chạy ứng dụng
 if __name__ == '__main__':
